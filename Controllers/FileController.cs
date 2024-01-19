44eedc490944b5b668b1dc084b30a529f56b9f53
Stefan Chen <zchen158@my.bcit.ca>
2024-01-19 15:22:46 -0800

    finish

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -14,10 +14,26 @@ public class FileController : Controller
     _env = env;
   }
 
-  public IActionResult Index()
-  {
+public IActionResult Index()
+{
     var path = Path.Combine(_env.ContentRootPath, "TextFiles");
-    ViewBag.Files = Directory.GetFiles(path);
+    var filePaths = Directory.GetFiles(path);
+    ViewBag.Files = Directory.GetFiles(path).Select(Path.GetFileName).ToArray();
     return View();
-  }
 }
+
+public IActionResult Content(string fileName)
+{
+    var path = Path.Combine(_env.ContentRootPath, "TextFiles", fileName);
+    if (System.IO.File.Exists(path))
+    {
+        ViewBag.Content = System.IO.File.ReadAllText(path);
+    }
+    else
+    {
+        ViewBag.Content = "File not found.";
+    }
+    return View();
+}
+}
+
